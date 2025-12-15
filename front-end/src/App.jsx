fd1a379e3a25f599c3fbdaa538b416a7447f6da5
terkoizmy <dimas.aryan021@gmail.com>
2025-12-15 22:27:42 +0700

    connect wallet wagmi viem

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,13 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useAccount } from 'wagmi';
 import LandingPage from './pages/LandingPage';
 import PhaserGame from './pages/PhaserGamePage';
+import { ConnectWallet } from './components/ConnectWallet';
+
+function ProtectedRoute({ children }) {
+  const { isConnected } = useAccount();
+  console.log(isConnected);
+  if (!isConnected) {
+    return <Navigate to="/" replace />;
+  }
+  return children;
+}
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/play" element={<PhaserGame />} />
+        <Route path="/connect" element={<ConnectWallet />} />
+        <Route
+          path="/play"
+          element={
+            <ProtectedRoute>
+              <PhaserGame />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
